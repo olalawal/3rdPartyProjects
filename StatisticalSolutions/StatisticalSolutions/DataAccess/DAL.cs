75284d5c69d0c245b707ce6163a5ca2c0ca80ebb
olalawal <ola_lawal@yahoo.com>
2015-01-23 20:39:58 -0600

    fixed reference problems added new method for adding client for testing

--- StatisticalSolutions/StatisticalSolutions/DataAccess/DAL.cs
+++ StatisticalSolutions/StatisticalSolutions/DataAccess/DAL.cs
@@ -13,33 +13,74 @@ namespace StatisticalSolutions.DataAccess
 
         internal bool CheckIfStudentIsRegistereredForSeminar(student model, int seminarid)
         {
+            StatisticalSolutionsContext db = new StatisticalSolutionsContext();
             try
             {
 
-                using (StatisticalSolutionsContext db = new StatisticalSolutionsContext())
+               
+                student user = db.students.FirstOrDefault(u => u.id == model.id);
+                // Check if user already exists
+                if (user == null)
                 {
-                    student user = db.students.FirstOrDefault(u => u.id == model.id);
-                    // Check if user already exists
-                    if (user == null)
-                    {
-                       //DO whatever work is required to check etc
+                    //DO whatever work is required to check etc
 
-                        return false;
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    return false;
                 }
+                else
+                {
+                    return false;
+                }
+
+
 
             }
             catch (Exception ex)
             {
-                
+
                 return false;
+            }
+            finally
+            { 
+              //add dispose here
             }
         }
 
+        //client model comes from chtml page or controller page
+        internal string addclient(client model)
+        {
+            StatisticalSolutionsContext db = new StatisticalSolutionsContext();
+            try
+            {
 
+                //first check if the client name is already in use
+
+                client client = db.clients.FirstOrDefault(u => u.Name == model.Name);
+
+
+                // Check if user already exists
+                if (client == null)
+                {
+                    //DO whatever work is required to check etc
+                    db.clients.Add(client);
+                    return "client added";
+                }
+                else
+                {
+                    return "A client with the same name already exists";
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return "failed";
+            }
+            finally
+            {
+                //add dispose here
+            }
+        }
     }
 }
