3b4994dc90a5ed70cdb36d9d5dce24a45a890bf0
shola-lawal <shola0080@hotmail.com>
2015-07-22 07:18:12 -0400

    Revert "added moms project"
    This reverts commit b332d2f66964d6a410250f9351e36860bcc93892.

--- LanguageInstitute/Scripts/dbo.Countries.data.sql
+++
@@ -1,4 +0,0 @@
-SET IDENTITY_INSERT [dbo].[Countries] ON
-INSERT INTO [dbo].[Countries] ([id], [Code], [Country]) VALUES (1, N'IN', N'India')
-INSERT INTO [dbo].[Countries] ([id], [Code], [Country]) VALUES (2, N'US', N'United States')
-SET IDENTITY_INSERT [dbo].[Countries] OFF
--- LanguageInstitute/Scripts/dbo.registrations.data.sql
+++
@@ -1,3 +0,0 @@
-SET IDENTITY_INSERT [dbo].[registrations] ON
-INSERT INTO [dbo].[registrations] ([id], [client_id], [student_id], [seminar_id], [Paid], [Attendend], [Attenddate], [Registerdate], [StartDate]) VALUES (1, 1, 1, 3, NULL, NULL, NULL, N'2015-04-19 09:35:58', N'2015-06-06 00:00:00')
-SET IDENTITY_INSERT [dbo].[registrations] OFF
