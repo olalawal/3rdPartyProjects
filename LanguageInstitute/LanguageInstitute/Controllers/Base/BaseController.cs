c2b68486e936889640eb3162d4caa2283454877e
olalawal <ola_lawal@yahoo.com>
2015-07-24 22:25:05 -0500

    added code

---
+++ LanguageInstitute/LanguageInstitute/Controllers/Base/BaseController.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Common.Logging;
+using System.IO;
+using LanguageInstitute.Helpers;
+using LanguageInstitute.Models;
+using LanguageInstitute.Filters;
+
+
+namespace LanguageInstitute.Controllers.Base
+{
+    [InitializeSimpleMembership]
+    public class BaseController : Controller
+    {
+
+        #region Instances
+        protected readonly ILog Log = LogManager.GetCurrentClassLogger();
+        #endregion
+
+
+        #region Properties
+
+        public string ErrorMessage { get; set; }
+
+        #endregion
+
+
+        #region Override Methods
+
+        /// <summary>
+        /// Method for show page notification on particular date,It will call on each action
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                Log.Info(m => m("Application URL - {0} ", Request.Path));
+                base.OnActionExecuting(filterContext);
+            }
+            catch (CustomException ex)
+            {
+                //if there is any error then put it into the log file...
+                Log.Error(m => m("Statistical Error Code - {0}", ex.ErrorCode));
+            }
+            catch (Exception ex)
+            {
+                //if there is any error then put it into the log file...
+                Log.Error(m => m("Statistical exception message - {0}", ex.Message));
+                Log.Error(m => m("Statistical exception stack trace - {0}", ex.StackTrace));
+            }
+        }
+
+        /// <summary>
+        /// Method called when any exception occured
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            try
+            {
+                Log.Info(m => m("Application URL - {0} ", Request.Path));
+                if (filterContext == null)
+                    return;
+
+                base.OnException(filterContext);
+                var ex = filterContext.Exception ?? new Exception("No further information exists.");
+                Session["StatisticalError"] = ex.Message;
+                Log.Error(m => m("Statistical exception message - {0}", ex.Message));
+                Log.Error(m => m("Statistical exception stack trace - {0}", ex.StackTrace));
+
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(
+                                                                    new
+                                                                    {
+                                                                        controller = "Base",
+                                                                        action = "StatisticalError",
+                                                                    
+                                                                    }
+                                                                    )
+                                                                );
+                filterContext.ExceptionHandled = true;
+                filterContext.HttpContext.Response.Clear();
+            }
+            catch (CustomException ex)
+            {
+                //if there is any error then put it into the log file...
+                Log.Error(m => m("Statistical Error Code - {0}", ex.ErrorCode));
+            }
+            catch (Exception ex)
+            {
+                //if there is any error then put it into the log file...
+                Log.Error(m => m("Statistical exception message - {0}", ex.Message));
+                Log.Error(m => m("Statistical exception stack trace - {0}", ex.StackTrace));
+            }
+        }
+
+        #endregion
+
+
+        #region Actions
+
+
+        /// <summary>
+        /// this will call when handled exception occured
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult StatisticalError() 
+        {
+            try
+            {
+                if (Session["StatisticalError"] == null)
+                {
+                    Session["StatisticalError"] = "No further information exists.";
+                }
+                Log.Info(m => m("Application URL - {0} ", Request.Path));
+                return View();
+            }
+            catch (CustomException ex)
+            {
+                return CustomExceptionCatcher(ex);
+            }
+            catch (Exception ex)
+            {
+                return SystemExceptionCatcher(ex);
+            }
+        }
+
+
+        /// <summary>
+        /// Catch the custom exception.
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public ActionResult CustomExceptionCatcher(CustomException ex)
+        {
+            //if there is any error then put it into the log file...
+            Log.Error(m => m("Custom Error Code - {0}", ex.ErrorCode));
+
+            //get mesg from the error code
+            var errorMesg = GetMessage(ex.ErrorCode);
+            Session["StatisticalError"] = errorMesg;
+            //return ThrowJsonError(ex, errorMesg);
+            return View("~/Views/Base/StatisticalError.cshtml");
+        }
+
+
+        /// <summary>
+        /// Catch the system thrown exception.
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public ActionResult SystemExceptionCatcher(Exception ex)
+        {
+            //if there is any error then put it into the log file...
+            Log.Error(m => m("Statistical exception message - {0}", ex.Message), ex);         
+
+            //Log your exception
+            var message = string.IsNullOrEmpty(ErrorMessage) ? ex.Message : ErrorMessage + " " + ex.Message;
+            Session["StatisticalError"] = message;
+            //return ThrowJsonError(ex, message);
+            return View("~/Views/Base/StatisticalError.cshtml");
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Method to get message for provided code
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public string GetMessage(string code)
+        {
+            try
+            {
+                Log.Info(m => m("Application URL - {0} ", Request.Path));
+                var dictMessages = (Dictionary<string, string>)HttpContext.Application["errorcode"];
+                return dictMessages[code];
+            }
+            catch (CustomException ex)
+            {
+                //if there is any error then put it into the log file...
+                Log.Error(m => m("Statistical Error Code - {0}", ex.ErrorCode));
+                return "";
+            }
+            catch (Exception ex)
+            {
+                //if there is any error then put it into the log file...
+                Log.Error(m => m("Statistical exception message - {0}", ex.Message));
+                Log.Error(m => m("Statistical exception stack trace - {0}", ex.StackTrace));
+                return "";
+            }
+        }
+
+
+
+     
+
+        #endregion
+
+    }
+}
---
+++ LanguageInstitute/LanguageInstitute/Filters/InitializeSimpleMembershipAttribute.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+using LanguageInstitute.Models;
+using Domain.StatisticalSolutions.Domain.Models.Context;
+using System.Web.Security;
+
+namespace LanguageInstitute.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class InitializeSimpleMembershipAttribute : ActionFilterAttribute
+    {
+        private static SimpleMembershipInitializer _initializer;
+        private static object _initializerLock = new object();
+        private static bool _isInitialized;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Ensure ASP.NET Simple Membership is initialized only once per app start
+            LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
+        }
+
+        private class SimpleMembershipInitializer
+        {
+            public SimpleMembershipInitializer()
+            {
+               // Database.SetInitializer<LanguageInstituteContext>(null);
+
+                try
+                {
+                    //using (var context = new LanguageInstituteContext())
+                    //{
+                    //    if (!context.UserProfiles.Exists())
+                    //    {
+                    //        // Create the SimpleMembership database without Entity Framework migration schema
+                    //        ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
+                    //    }
+                    //}
+
+                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+
+                  
+
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+                }
+            }
+        }
+    }
+}
