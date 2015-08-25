b4f92c9a20e6acc2aa813ca7c37a1bff1be1df8e
olalawal <ola_lawal@yahoo.com>
2015-08-24 21:36:21 -0500

    addeed new design for language institute

---
+++ LanguageInstitute/LanguageInstitute/obj/Release/AspnetCompileMerge/TempBuildDir/Scripts/Statistical.Alert.js
@@ -0,0 +1,40 @@
+﻿Statistical_Alert = (function () {
+
+    // class function a.k.a. constructor
+    function cls() {
+        // private instance fields
+        var self = this;
+
+    }
+
+    // public static method
+    cls.success = function (msg) {
+        $('.bottom-right').notify({
+            message: { html: "<b>" + msg.title + "</b> " + msg.text },
+            type: 'success'
+        }).show(); // for the ones that aren't closable and don't fade out there is a .close() function.
+    };
+
+    cls.info = function (msg) {
+        $('.bottom-right').notify({
+            message: { html: "<b>" + msg.title + "</b> " + msg.text },
+            type: 'info'
+        }).show(); // for the ones that aren't closable and don't fade out there is a .close() function.
+    };
+
+    cls.warning = function (msg) {
+        $('.bottom-right').notify({
+            message: { html: "<b>" + msg.title + "</b> " + msg.text },
+            type: 'warning'
+        }).show(); // for the ones that aren't closable and don't fade out there is a .close() function.
+    };
+
+    cls.error = function (msg) {
+        $('.bottom-right').notify({
+            message: { html: "<b>" + msg.title + "</b> " + msg.text },
+            type: 'danger'
+        }).show(); // for the ones that aren't closable and don't fade out there is a .close() function.
+    };
+
+    return cls;
+})();
